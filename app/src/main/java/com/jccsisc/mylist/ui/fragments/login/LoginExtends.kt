45d15cf324f240237da476ae9b3d1abbdf5cea8d
JccsISC <silva.jccs18@gmail.com>
2022-06-10 08:54:15 -0500

    Autentitacion con vissta login predeterminada

--- app/src/main/java/com/jccsisc/mylist/ui/fragments/login/LoginExtends.kt
+++ app/src/main/java/com/jccsisc/mylist/ui/fragments/login/LoginExtends.kt
@@ -1,6 +1,11 @@
 package com.jccsisc.mylist.ui.fragments.login
 
+import android.app.Activity.RESULT_OK
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.jccsisc.mylist.R
 import com.jccsisc.mylist.ui.activities.home.HomeActivity
 import com.jccsisc.mylist.utils.goToActivity
@@ -9,6 +14,8 @@ import com.jccsisc.mylist.utils.setOnSingleClickListener
 fun LoginFragment.initElements() = with(mBinding) {
 
     btnBack.setOnSingleClickListener { findNavController().popBackStack() }
+
+    configAuth()
 
     btnLogin.setOnSingleClickListener {
         activity?.let {
--- app/src/main/java/com/jccsisc/mylist/ui/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/jccsisc/mylist/ui/fragments/login/LoginFragment.kt
@@ -1,12 +1,61 @@
 package com.jccsisc.mylist.ui.fragments.login
 
+import android.app.Activity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.jccsisc.mylist.R
 import com.jccsisc.mylist.common.base.BaseFragment
 import com.jccsisc.mylist.databinding.FragmentLoginBinding
+import com.jccsisc.mylist.ui.activities.home.HomeActivity
+import com.jccsisc.mylist.utils.goToActivity
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginVM>(LoginVM::class) {
+
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
     override fun getLayout() = R.layout.fragment_login
 
     override fun initView() { initElements() }
+
+    fun configAuth() {
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                requireActivity().goToActivity<HomeActivity>()
+            } else {
+
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    val response = IdpResponse.fromResultIntent(it.data)
+
+                    if (it.resultCode == Activity.RESULT_OK) {
+                        val user = FirebaseAuth.getInstance()
+                        if (user != null) {
+                            requireActivity().goToActivity<HomeActivity>()
+                        }
+                    }
+
+                }.launch(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build())
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
 }
